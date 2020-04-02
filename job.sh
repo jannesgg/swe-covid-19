1b3b4ba25a430f53ded02b8c4e90e1842177fdee
Jannes <jurie.germishuys@combine.se>
2020-04-02 09:46:07 +0200

    Latest update

---
+++ job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /Users/juriegermishuys/PycharmProjects/gatsby-map
+git pull
+mv src/data/time_series/* ../covid/*
+cd /Users/juriegermishuys/PycharmProjects/covid
+git add *
+git commit -m "Latest update"
+git push -u origin master
