b494b1d440b66b245812eeb2831c2c58ab884256
Jannes <jurie.germishuys@combine.se>
2020-04-02 11:40:10 +0200

    Remove shell script

--- job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /Users/juriegermishuys/PycharmProjects/gatsby-map
-git pull
-mv src/data/time_series/* ../covid/
-cd /Users/juriegermishuys/PycharmProjects/covid
-git add *
-git commit -m "Latest update"
-git push -u origin master
